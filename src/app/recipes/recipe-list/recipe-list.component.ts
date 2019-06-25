ad444fa9d0b02bb9858641d409a66e6063a14162
Oscar Larriega <oscar.larriega@globant.com>
2019-06-25 12:57:25 -0500

    Add dynamic navigation with routerLink

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.services';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,14 +11,9 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService,
-              private router: Router) { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelect(id: number) {
-    this.router.navigate(['/recipes', id]);
   }
 }
