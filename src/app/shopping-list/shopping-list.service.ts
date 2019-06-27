b3d23055518168809b06d1463611424743c4187b
Oscar Larriega <oscar.larriega@globant.com>
2019-06-27 11:58:33 -0500

    Add form Template Driven for ShoppingListEdit component

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,11 +3,16 @@ import { Subject } from 'rxjs/Subject';
 
 export class ShoppinglistService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 15)
     ];
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
 
     getIngredients() {
         return this.ingredients.slice();
@@ -23,6 +28,16 @@ export class ShoppinglistService {
             this.ingredients.push(ingredient);
         } */
         this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    deleteIngredient(index: number) {
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
